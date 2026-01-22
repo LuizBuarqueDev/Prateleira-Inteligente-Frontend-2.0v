82a3612a5d3ca874b059ff4891640c6732e156ce
LuizBuarqueDev <luizgabrielbuarque@gmail.com>
2026-01-22 20:32:29 -0300

    feat: replace lucide-angular with lucide-react and update sidenav component for improved navigation

--- Prateleira-Inteligente-Frontend-2.0v/src/app/shared/layout/sidebar/sidenav.ts
+++ Prateleira-Inteligente-Frontend-2.0v/src/app/shared/layout/sidebar/sidenav.ts
@@ -1,16 +1,18 @@
-import {Component} from '@angular/core';
-import {MatButtonModule} from '@angular/material/button';
-import {MatSidenavModule} from '@angular/material/sidenav';
-
-/**
- * @title Autosize sidenav
- */
+import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterModule } from '@angular/router';
 @Component({
-selector: 'app-sidenav',
+  selector: 'app-sidenav',
+  standalone: true,
   templateUrl: 'sidenav.html',
   styleUrl: 'sidenav.scss',
-  imports: [MatSidenavModule, MatButtonModule],
+  imports: [
+    MatSidenavModule,
+    MatButtonModule,
+    MatIconModule,
+    RouterModule
+  ]
 })
-export class Sidenav {
-  showFiller = false;
-}
+export class Sidenav {}
