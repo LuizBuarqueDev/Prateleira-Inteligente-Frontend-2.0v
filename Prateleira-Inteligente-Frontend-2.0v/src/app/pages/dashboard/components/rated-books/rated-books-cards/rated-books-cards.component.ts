3c6835be2e2bfd4a3fdbcc1131925e6d9197a65a
LuizBuarqueDev <luizgabrielbuarque@gmail.com>
2026-02-08 00:05:11 -0300

    feat: update rated-books component to dynamically display book details and add no-cover image

--- Prateleira-Inteligente-Frontend-2.0v/src/app/pages/dashboard/components/rated-books/rated-books-cards/rated-books-cards.component.ts
+++ Prateleira-Inteligente-Frontend-2.0v/src/app/pages/dashboard/components/rated-books/rated-books-cards/rated-books-cards.component.ts
@@ -1,8 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { BookService } from '../../../../../http/services/book.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-rated-books-cards',
   templateUrl: './rated-books-cards.component.html',
   styleUrl: './rated-books-cards.component.scss',
+  standalone: true,
+  imports: [CommonModule],
 })
-export class RatedBooksCardsComponent {}
+export class RatedBooksCardsComponent {
+  books$ = inject(BookService).getSimplifiedBooks();
+}
