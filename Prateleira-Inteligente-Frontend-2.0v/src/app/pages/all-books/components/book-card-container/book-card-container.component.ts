5c28c3aff4cfc136b39f881917b5e07953eff0d2
LuizBuarqueDev <luizgabrielbuarque@gmail.com>
2026-02-24 21:20:51 -0300

    feat: implement book card component and loading/error states in book card container

--- Prateleira-Inteligente-Frontend-2.0v/src/app/pages/all-books/components/book-card-container/book-card-container.component.ts
+++ Prateleira-Inteligente-Frontend-2.0v/src/app/pages/all-books/components/book-card-container/book-card-container.component.ts
@@ -1,15 +1,15 @@
-import { Component, inject } from "@angular/core";
+import { Component, inject } from '@angular/core';
 import { BookService } from '@services/book.service';
-import { CommonModule } from "@angular/common";
+import { CommonModule } from '@angular/common';
+import { BookCardComponent } from './book-card/book-card.component';
 
 @Component({
-  selector: "app-book-card-container",
+  selector: 'app-book-card-container',
   standalone: true,
-  templateUrl: "./book-card-container.component.html",
-  styleUrls: ["./book-card-container.component.scss"],
-  imports: [CommonModule,],
+  templateUrl: './book-card-container.component.html',
+  styleUrls: ['./book-card-container.component.scss'],
+  imports: [CommonModule, BookCardComponent],
 })
-
 export class BookCardContainerComponent {
   books = inject(BookService).simplifiedBooks;
 }
--- Prateleira-Inteligente-Frontend-2.0v/src/app/pages/all-books/components/book-card-container/book-card/book-card.component.ts
+++ Prateleira-Inteligente-Frontend-2.0v/src/app/pages/all-books/components/book-card-container/book-card/book-card.component.ts
@@ -1,10 +1,14 @@
-import { Component } from "@angular/core";
+import { CommonModule } from '@angular/common';
+import { Component, input } from '@angular/core';
+import { BookSimplified } from '@app/shared/models/simplified/book-simplified.model';
 
 @Component({
-  selector: "app-book-card",
+  selector: 'app-book-card',
   standalone: true,
-  templateUrl: "./book-card.component.html",
-  styleUrls: ["./book-card.component.scss"],
+  templateUrl: './book-card.component.html',
+  styleUrls: ['./book-card.component.scss'],
+  imports: [CommonModule],
 })
 export class BookCardComponent {
+  book = input<BookSimplified>();
 }
