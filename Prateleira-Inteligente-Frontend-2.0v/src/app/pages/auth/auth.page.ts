5d77f28b03066a906794bde31a5a34f115c6e68b
LuizBuarqueDev <luizgabrielbuarque@gmail.com>
2026-04-06 23:29:07 -0300

    feat: update auth page layout and styles for improved user experience

--- Prateleira-Inteligente-Frontend-2.0v/src/app/pages/auth/auth.page.ts
+++ Prateleira-Inteligente-Frontend-2.0v/src/app/pages/auth/auth.page.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-auth',
   templateUrl: 'auth.page.html',
   styleUrls: ['auth.page.scss'],
-  imports: [],
+  imports: [MatIcon],
 })
 export class AuthPage {}
