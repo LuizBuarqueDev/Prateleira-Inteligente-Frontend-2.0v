1be88d94d2704e6e17ffb2d22ca0eab44b6f58e9
LuizBuarqueDev <luizgabrielbuarque@gmail.com>
2026-04-07 20:37:50 -0300

    feat: enhance auth page layout and styles for improved usability and aesthetics

--- Prateleira-Inteligente-Frontend-2.0v/src/app/pages/auth/auth.page.ts
+++ Prateleira-Inteligente-Frontend-2.0v/src/app/pages/auth/auth.page.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
-import { MatIcon } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-auth',
   templateUrl: 'auth.page.html',
   styleUrls: ['auth.page.scss'],
-  imports: [MatIcon],
+  standalone: true,
+  imports: [MatFormFieldModule, MatInputModule, MatIconModule, MatButtonModule],
 })
 export class AuthPage {}
