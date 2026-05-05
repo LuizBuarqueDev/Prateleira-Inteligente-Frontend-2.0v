8f8246330f8ea955e22a246f7c20704add85a498
LuizBuarqueDev <luizgabrielbuarque@gmail.com>
2026-05-05 12:13:02 -0300

    feat: implement reactive form for authentication with email and password fields

--- Prateleira-Inteligente-Frontend-2.0v/src/app/pages/auth/auth.page.ts
+++ Prateleira-Inteligente-Frontend-2.0v/src/app/pages/auth/auth.page.ts
@@ -3,12 +3,22 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { FormsModule, FormBuilder, ReactiveFormsModule, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
   templateUrl: 'auth.page.html',
   styleUrls: ['auth.page.scss'],
   standalone: true,
-  imports: [MatFormFieldModule, MatInputModule, MatIconModule, MatButtonModule],
+  imports: [MatFormFieldModule, MatInputModule, MatIconModule, MatButtonModule, FormsModule, ReactiveFormsModule],
 })
-export class AuthPage {}
+export class AuthPage {
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: [''],
+      password: [''],
+    });
+  }
+}
